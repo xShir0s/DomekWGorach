2d6b1dce51c67657a04516a06d266c6737925e44
xShir0s <mtodys2006@gmail.com>
2024-10-16 09:28:06 +0200

    dodanie dokumentacji funkcji

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,12 +10,46 @@
 
         int likes = 0;
 
+        /******************************************************* 
+
+            nazwa funkcji:        private void IncrementLikes(object sender, EventArgs e)
+
+                parametry wejściowe:  object sender - zawiera odwołanie do obiektu
+                                      EventArgs e - przekazuje obiekt specyficzny dla obsługiwanego zdarzenia
+                                       
+
+                wartość zwracana:     brak
+
+                informacje:           Inkrementuje wartość likes, i podstawia do wartości LblLikeCounter.Text
+
+                autor:                Miłosz Todys 
+
+            ****************************************************/
+
+
         private void IncrementLikes(object sender, EventArgs e)
         {
             likes++;
             LblLikeCounter.Text = $"Polubień: {likes}";
 
         }
+
+        /******************************************************* 
+
+        nazwa funkcji:               private void DecrementLikes(object sender, EventArgs e)
+
+
+        parametry wejściowe:  object sender - zawiera odwołanie do obiektu
+                       EventArgs e - przekazuje obiekt specyficzny dla obsługiwanego zdarzenia
+
+
+        wartość zwracana:     brak
+
+        informacje:           deInkrementuje wartość likes, i podstawia do wartości LblLikeCounter.Text
+
+        autor:                Miłosz Todys 
+
+        ****************************************************/
         private void DecrementLikes(object sender, EventArgs e)
         {
             if (likes == 0)
